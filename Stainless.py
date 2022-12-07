e121b3bab8c52ba06155c567a03f6c49248a4b4e
Jonah Wright <robot2boy@icloud.com>
2022-12-06 21:09:56 -0800

    Changed scoreboard and message
    Added average time and confirmed messages are in the #wordle channel

--- Stainless.py
+++ Stainless.py
@@ -4,9 +4,10 @@ import asyncio
 from urllib.parse import urlparse, parse_qs
 import pickle
 import os
+import datetime
 
 #import the token from a file named bot_token
-from bot_token import token, prefix
+from bot_token import token, prefix, channel_id
 scoreboard = {}
 with open('exported_scoreboard.pkl', 'rb') as f:
     scoreboard = pickle.load(f)
@@ -33,51 +34,55 @@ class MyClient(discord.Client):
        # we do not want the bot to reply to itself
         if message.author.id == self.user.id:
             return     
-        if message.content[:5] == 'https':
-            url = urlparse(message.content)
-            query = parse_qs(url.query)
-            date = query['d'][0]
-            time = query['t'][0]
-            user_id = message.author.id
+        print("message detected in " + str(message.channel.id))
+        if message.channel.id == channel_id:
+            print("message in #wordle detected")
+            if message.content[:5] == 'https':
+                url = urlparse(message.content)
+                query = parse_qs(url.query)
+                date = query['d'][0]
+                time = query['t'][0]
+                user_id = message.author.id
 
-            if scoreboard.get(user_id) is None:
-                scoreboard[user_id] = [1, int(time), {date:time}]
-            else:
-                user_list = scoreboard[user_id]
-                user_list[0] += 1
-                user_list[1] += int(time)
-                user_list[2].update({date:time})
+                if scoreboard.get(user_id) is None:
+                    scoreboard[user_id] = [1, int(time), {date:time}]
+                else:
+                    user_list = scoreboard[user_id]
+                    user_list[0] += 1
+                    user_list[1] += int(time)
+                    user_list[2].update({date:time})
 
 
-            await message.reply('Date: ' + date + ' Time: ' + time, mention_author=True)
-            return
-        if message.content.startswith(prefix+'sb'):
-            temp = {}
-            for x in scoreboard:
-                temp[x] = [scoreboard[x][0], scoreboard[x][1], scoreboard[x][2]]
-            #print('temp')
-            #print(temp)
-            #print('sb')
-            #print(scoreboard)
-            while len(temp) != 0:
-                lowest = list(temp.keys())[0]
-                #print(type(lowest))
-                #print(lowest)
-                for i in temp:
-                    if temp[i][1] < temp[lowest][1]:
-                        lowest = i
-                userid = lowest
-                #print('userid')
-                #print(userid)
-                user = await self.fetch_user(userid)
-                username = user.display_name
-                #print('username')
-                #print(username)
-                await message.channel.send(username + ": " + str(scoreboard[lowest][1]))
-                temp.pop(lowest)
+                await message.reply('Date: ' + date + ' Time: ' + time, mention_author=True)
+                return
+            if message.content.startswith(prefix+'sb'):
+                temp = {}
+                for x in scoreboard:
+                    temp[x] = [scoreboard[x][0], scoreboard[x][1], scoreboard[x][2]]
+                #print('temp')
+                #print(temp)
+                #print('sb')
+                #print(scoreboard)
+                while len(temp) != 0:
+                    lowest = list(temp.keys())[0]
+                    #print(type(lowest))
+                    #print(lowest)
+                    for i in temp:
+                        if temp[i][1] < temp[lowest][1]:
+                            lowest = i
+                    userid = lowest
+                    #print('userid')
+                    #print(userid)
+                    user = await self.fetch_user(userid)
+                    username = user.display_name
+                    #print('username')
+                    #print(username)
+                    await message.channel.send(username + ": " + str(scoreboard[lowest][1]) + " Average time: " + str(datetime.timedelta(seconds=(scoreboard[lowest][1]/scoreboard[lowest][0]))))
+                    temp.pop(lowest)
 
     @tasks.loop(seconds=60)    
     async def my_background_task(self):
+    #every 60s exports the scoreboard
         if os.path.exists('exported_scoreboard.pkl'):
             with open('exported_scoreboard.pkl', 'wb') as f:
                 pickle.dump(scoreboard, f)
@@ -87,13 +92,13 @@ class MyClient(discord.Client):
 
     @my_background_task.before_loop
     async def before_my_task(self):
+    #loads the scoreboard from the .pkl file
         if os.path.exists('exported_scoreboard.pkl'):
             if os.path.getsize('exported_scoreboard.pkl') > 0:
                 with open('exported_scoreboard.pkl', 'rb') as f:
                     scoreboard = pickle.load(f)
                     f.close()
         await self.wait_until_ready()  # wait until the bot logs in
-        
 
 intents = discord.Intents.default()
 intents.message_content = True
