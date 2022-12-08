0f2a6ddc8eddf18dc1ba81fc959ead2f2a9a29fc
Jonah Wright <robot2boy@icloud.com>
2022-12-08 00:10:19 -0800

    Update Stainless.py

--- Stainless.py
+++ Stainless.py
@@ -9,8 +9,8 @@ import datetime
 #import the token from a file named bot_token
 from config import token, prefix, channel_id
 start = datetime.datetime.now()
-scoreboard = {}
-points = {}
+global scoreboard
+global points
 if os.path.getsize('exported_scoreboard.pkl') > 0:
     with open('exported_scoreboard.pkl', 'rb') as f:
         scoreboard = pickle.load(f)
@@ -33,94 +33,119 @@ async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
     print('------')
     await bot.add_cog(Export(bot))
-
-@bot.event
-async def on_message(message: discord.Message):
-    # we do not want the bot to reply to itself
-    if message.author.id == bot.user.id:
-        return
-    print("message detected in " + str(message.channel.id))
-    if message.channel.id == channel_id or message.channel.id == 653758045708615683:
-        print("message in #wordle detected")
-        if message.content[:5] == 'https':
-            url = urlparse(message.content)
-            query = parse_qs(url.query)
-            date = query['d'][0]
-            time = query['t'][0]
-            user_id = message.author.id
-
-            if scoreboard.get(user_id) is None:
-                scoreboard[user_id] = [1, int(time), {date:time}]
-            else:
-                user_list = scoreboard[user_id]
-                user_list[0] += 1
-                user_list[1] += int(time)
-                user_list[2].update({date:time})
+    await bot.add_cog(Admin(bot))
+    await bot.add_cog(Wordle(bot))
 
 
-            await message.reply('Date: ' + date + ' Time: ' + time, mention_author=True)
-            return
-    else:
-        user_id = message.author.id
-        if points.get(user_id) is None:
-            points[user_id] = 1
-        else:
-            points[user_id] += 1
-            user = await bot.fetch_user(user_id)
-            username = user.display_name
-            print("added point to " + username + " new point total: " + str(points[user_id]))
-    await bot.process_commands(message)
-
-@bot.command()
-async def sb(ctx):
-    """Shows the scoreboard"""
-    temp = {}
-    for x in scoreboard:
-        temp[x] = [scoreboard[x][0], scoreboard[x][1], scoreboard[x][2]]
-    #print('temp')
-    #print(temp)
-    #print('sb')
-    #print(scoreboard)
-    while len(temp) != 0:
-        lowest = list(temp.keys())[0]
-        #print(type(lowest))
-        #print(lowest)
-        for i in temp:
-            if temp[i][1] < temp[lowest][1]:
-                lowest = i
-        userid = lowest
-        #print('userid')
-        #print(userid)
-        user = await bot.fetch_user(userid)
-        username = user.display_name
-        #print('username')
-        #print(username)
-        average_time = scoreboard[lowest][1]/scoreboard[lowest][0]
-        await ctx.message.channel.send(username + ": " + str(scoreboard[lowest][1]) + " Average time: " + str(datetime.timedelta(seconds=average_time)))
-        temp.pop(lowest)
-
-@bot.command()
-async def uptime(ctx):
-    now = datetime.datetime.now()
-    diffrence = now - start
-    await ctx.message.channel.send("Running for: " + str(diffrence))
-
-@bot.command()
-@commands.is_owner()
-async def kill(ctx):
-    await bot.remove_cog('Export')
-    await bot.close()
-
-class Export(commands.Cog):
+class Wordle(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+    @commands.command(aliases=['sb'])
+    async def scoreboard(self, ctx):
+        """Displays the scoreboard"""
+        print("Command scoreboard detected.")
+        temp = {}
+        for x in scoreboard:
+            temp[x] = [scoreboard[x][0], scoreboard[x][1], scoreboard[x][2]]
+        print(temp)
+        #print('temp')
+        #print(temp)
+        #print('sb')
+        #print(scoreboard)
+        while len(temp) != 0:
+            lowest = list(temp.keys())[0]
+            #print(type(lowest))
+            #print(lowest)
+            for i in temp:
+                if temp[i][1] < temp[lowest][1]:
+                    lowest = i
+            userid = lowest
+            #print('userid')
+            #print(userid)
+            user = await bot.fetch_user(userid)
+            username = user.display_name
+            #print('username')
+            #print(username)
+            average_time = scoreboard[lowest][1]/scoreboard[lowest][0]
+            await ctx.message.channel.send(username + ": " + str(scoreboard[lowest][1]) + " Average time: " + str(datetime.timedelta(seconds=average_time)))
+            print(username + ": " + str(scoreboard[lowest][1]) + " Average time: " + str(datetime.timedelta(seconds=average_time)))
+            temp.pop(lowest) 
+        return
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == bot.user.id:
+            return
+        print("message detected in " + str(message.channel.id))
+        if message.channel.id == channel_id or message.channel.id == 653758045708615683:
+            print("message in #wordle detected")
+            if message.content[:5] == 'https':
+                url = urlparse(message.content)
+                query = parse_qs(url.query)
+                date = query['d'][0]
+                time = query['t'][0]
+                user_id = message.author.id
+
+                if scoreboard.get(user_id) is None:
+                    scoreboard[user_id] = [1, int(time), {date:time}]
+                else:
+                    user_list = scoreboard[user_id]
+                    user_list[0] += 1
+                    user_list[1] += int(time)
+                    user_list[2].update({date:time})
+
+
+                await message.reply('Date: ' + date + ' Time: ' + time, mention_author=True)
+                return
+        else:
+            user_id = message.author.id
+            if points.get(user_id) is None:
+                points[user_id] = 1
+            else:
+                points[user_id] += 1
+                user = await bot.fetch_user(user_id)
+                username = user.display_name
+                print("added point to " + username + " new point total: " + str(points[user_id]))
+                return              
+
+class Admin(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    @commands.command(hidden=True)
+    @commands.is_owner()
+    async def kill(self, ctx):
+        """Kills the program"""
+        await bot.remove_cog('Export')
+        await bot.close()
+
+    @commands.command()
+    async def uptime(self, ctx):
+        """Displays how long the bot has been running"""
+        now = datetime.datetime.now()
+        diffrence = now - start
+        await ctx.message.channel.send("Running for: " + str(diffrence))
+        return
+
+class Export(commands.Cog):
+    def __init__(self, bot1):
+        self.bot = bot1
         self.export.start()
 
     def cog_unload(self):
+        with open('exported_scoreboard.pkl', 'wb') as f:
+            pickle.dump(scoreboard, f)
+            f.close()
+            print('exported scoreboard')
+            print(scoreboard)
+        with open('points.pkl', 'wb') as f:
+            pickle.dump(points, f)
+            f.close()
+            print('exported points')
+            print(points)
         self.export.cancel()
 
     @tasks.loop(seconds=60)
-    async def export(bot):
+    async def export(self):
     #every 60s exports the scoreboard
         if os.path.exists('exported_scoreboard.pkl'):
             with open('exported_scoreboard.pkl', 'wb') as f:
@@ -149,9 +174,5 @@ class Export(commands.Cog):
                 with open('points.pkl', 'rb') as f:
                     scoreboard = pickle.load(f)
                     f.close()
-
-
-
-
 
 bot.run(token)
