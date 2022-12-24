c1d4fb0221ea2918ea64fc57d3bc126b52a75e38
Jonah Wright <robot2boy@icloud.com>
2022-12-23 16:56:50 -0800

    Added a class module and updated stainless to use it

---
+++ Classes.py
@@ -0,0 +1,126 @@
+class Server:
+    def __init__(self) -> None:
+        self.users = {}#dict of all the users
+        self.days = {}#dict of all the days
+    def add_score(self, userid, date, time):
+        """Adds a score to the user"""
+        if self.users.get(userid) is None:#create new user
+            user = User(userid, 0, 0, {})
+            self.users.update({userid:user})
+        user = self.users.get(userid)
+        prev_score = user.add_day(date, time)
+        #portion of code that handles days
+        if self.days.get(date) is None:
+            day = Day(userid, time)
+            self.days.update({date:day})
+            print("added date")
+        else:
+            day = self.days.get(date)
+            day.add_user(userid, time)
+            print("else")
+        print(self.days)
+        if prev_score == 0:
+            return f"Score: {time} Date: {date}"
+        return f"Previous Score: {prev_score} New Score: {time} Date: {date}"
+    def get_scoreboard(self):
+        """Returns a dict with the score and then the user ID"""
+        score_sort = {}
+        for user_id, user in self.users.items():
+            print(self.days)
+            output = user.get_total_time()
+            print(output)
+            total_score = self.get_score(user_id)
+            print(total_score)
+            score_sort.update({total_score:user_id})
+        return score_sort
+    def user_score(self, userid):
+        """Returns an individual users score"""
+        user = self.users.get(userid)
+        if user is None:
+            return "No User with that name"
+        day_dict = user.get_days_participated()
+        message = f"User's ranked score is {self.get_score(userid)}\n"
+        for day, score in day_dict.items():
+            message += f"User scored {score} on {day}\n"
+        return message
+    def import_user(self):
+        pass
+    def get_score(self, userid):
+        """Returns the ranked score of the User"""
+        total_score = 0
+        day_list = []
+        user = self.users.get(userid)
+        day_list = user.get_days_participated_list()
+        for day in day_list:
+            total_score += self.days[day].get_score(userid)
+        return total_score
+    def get_avg_time(self, userid):
+        user = self.users.get(userid)
+        if user is None:
+            return 0
+        return user.get_avg_time()
+
+
+class User:
+    def __init__(self, userid, num_days, total_time, days_participated) -> None:
+        self.userid = userid #userid of the userclass
+        self.num_days = num_days #num of days the user has participated in total
+        self.total_time = total_time #the total amount of time the user has participated in
+        self.days_participated = days_participated #a dictionary with all the dates with a score
+    def add_day(self, date, time):
+        """Adds a day to the user"""
+        previous_time = self.days_participated.get(date, 0)
+        total = self.total_time - previous_time
+        self.days_participated.update({date:time})
+        self.num_days = len(self.days_participated)
+        total += int(time)
+        self.total_time = total
+        if previous_time != 0:
+            return previous_time
+        return 0
+
+
+
+    def remove_day(self):
+        pass
+    def get_info(self):
+        pass
+    def get_num_days(self):
+        return self.num_days
+    def get_total_time(self):
+        return self.total_time
+    def get_days_participated(self):
+        return self.days_participated
+    def get_days_participated_list(self):
+        return list(self.days_participated.keys())
+    def get_avg_time(self):
+        return self.total_time / self.num_days
+
+class Day:
+    def __init__(self, userid, score) -> None:
+        self.users = {userid:score}
+        self.scores = {userid:100}
+    def add_user(self, userid, time):
+        """Adds a user and their score to the day"""
+        self.users.update({userid:time})
+        self.update()
+        return
+    def update(self):
+        """Updates the rankings for the day"""
+        temp = list(self.users.keys())
+        lowest = temp[0]
+        lowest_time = self.users.get(lowest)
+        print(f"first lowest time {lowest_time}")
+        for i in self.users:
+            test = self.users.get(i)
+            if lowest_time > test:
+                lowest = i
+                lowest_time = test
+                print(f"New lowest time {lowest_time}")
+        for i in self.users:
+            score = (lowest_time/self.users.get(i))  * 100
+            self.scores.update({i:score})
+        return
+    def get_score(self, userid):
+        """Returns the ranked score of the given user"""
+        return self.scores.get(userid)
